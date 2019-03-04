b44ecd6dee20ef0ae7b10429897acad8ca14707c
GreyWayfarer <l.box@bk.ru>
2019-03-04 17:52:05 +0700

    Add .gitignore, .clang-format, makefile, main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *file;
+    struct data {
+        struct triangle {
+        };
+        struct circle {
+        };
+        char name[10] 
+    };
+    
+    file = fopen("fscanf.txt", "r");
+    
+    while (name[i] = fgetc(file) != EOF) {
+        if (name[i] == "triangle"
+
+   //while(fscanf (file, "%d%*c%d%*c%d\n", &(data.x), &(data.y), &(data.z)));
+        
